7a5fcc213f01eae263e16cc2cc0e51100e3083cd
blackwolf08 <dhamasunny98@gmail.com>
2023-10-15 02:08:04 -0400

    feat: removed sw.js

--- src/main.jsx
+++ src/main.jsx
@@ -25,14 +25,3 @@ root.render(
     </PersistGate>
   </ReduxProvider>
 );
-
-// if ('serviceWorker' in navigator) {
-//   navigator.serviceWorker
-//     .register('/service-worker.js')
-//     .then((registration) => {
-//       console.log('Service Worker registered with scope:', registration.scope);
-//     })
-//     .catch((error) => {
-//       console.log('Service Worker registration failed:', error);
-//     });
-// }
